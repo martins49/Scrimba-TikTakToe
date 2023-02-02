50c2313fb1c3a24a207d45ab6e4e3abfdf19b0e1
martins49 <martobusayo49@gmail.com>
2023-02-02 03:54:27 +0000

    initial commit to file

---
+++ index.js
@@ -0,0 +1,79 @@
+console.log("TikTakToe")
+const CATS = "🐱"
+const MONKEYS = "🐒"
+
+let catsPicks = [];
+let monkeysPicks = [];
+
+let turn = CATS
+const squareEl = document.querySelectorAll(".grid div")
+
+console.log(squareEl.length)
+// squareEl.addEventListener("click", function(){
+//     console.log("i was clicked")
+// })
+
+for (let i = 0; i < squareEl.length; i++) {
+    squareEl[i].addEventListener("click", takeTurn)
+}
+
+function takeTurn() {
+    this.textContent = turn
+
+    if (turn == CATS) {
+        catsPicks.push(parseInt(this.id))
+        turn = MONKEYS
+    }
+    else {
+        monkeysPicks.push(parseInt(this.id))
+        turn = CATS
+    }
+    //to remove the event listener
+    this.removeEventListener("click", takeTurn)
+    console.log(catsPicks)
+    console.log(monkeysPicks)
+    console.log(this.textContent)
+
+    // this.textContent = (turn == CATS) ? CATS : MONKEYS
+    // turn = (turn == CATS) ? MONKEYS : CATS
+    //Determine index of Square
+    //change paragraph text content 🐱 or 🐒
+    //check for a winner
+    //check for draw
+
+    checkWinner()
+    // checkDraw()
+}
+
+function checkWinner() {
+    let display = ""
+    if (JSON.stringify(catsPicks) == JSON.stringify([0, 1, 2]) ||
+        JSON.stringify(catsPicks) == JSON.stringify([3, 4, 5]) ||
+        JSON.stringify(catsPicks) ==JSON.stringify([6, 7, 8]) ||
+        JSON.stringify(catsPicks) == JSON.stringify([0, 4, 8]) ||
+        JSON.stringify(catsPicks) == JSON.stringify([2, 4, 6]) ||
+        JSON.stringify(catsPicks) == JSON.stringify([0, 3, 6]) ||
+        JSON.stringify(catsPicks) == JSON.stringify([1, 4, 7]) ||
+        JSON.stringify(catsPicks) == JSON.stringify([2, 5, 8]) ||
+        JSON.stringify(catsPicks) == JSON.stringify([6, 4, 2])
+    )
+        display = "player 1 has won🐱🐱🐱"
+    else if (JSON.stringify(monkeysPicks) === JSON.stringify([0, 1, 2]) ||
+        JSON.stringify(monkeysPicks) === JSON.stringify([3, 4, 5]) ||
+        JSON.stringify(monkeysPicks) === JSON.stringify([6, 7, 8]) ||
+        JSON.stringify(monkeysPicks) === JSON.stringify([0, 4, 8]) ||
+        JSON.stringify(monkeysPicks) === JSON.stringify([2, 4, 6]) ||
+        JSON.stringify(monkeysPicks) === JSON.stringify([0, 3, 6]) ||
+        JSON.stringify(monkeysPicks) === JSON.stringify([1, 4, 7]) ||
+        JSON.stringify(monkeysPicks) === JSON.stringify([2, 5, 8]) ||
+        JSON.stringify(monkeysPicks) === JSON.stringify([6, 4, 2])
+
+    )
+        display = "player 2 has won🐒🐒🐒"
+    console.log(display);
+}
+
+
+// function checkDraw() {
+
+// }
